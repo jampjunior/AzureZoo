f38122d2e9f8d9665e8cf73883081168b38c7fb9
Borja <jampdesigner@gmail.com>
2017-07-04 17:37:32 +0100

    js de agregar ok falta ver porque no me conecta

--- ZooAzure/ZooAzure/Controllers/TiposAnimalesController.cs
+++ ZooAzure/ZooAzure/Controllers/TiposAnimalesController.cs
@@ -35,24 +35,116 @@ namespace ZooAzure.Controllers
         }
 
         // GET: api/TiposAnimales/5
-        public string Get(int id)
+        public RespuestaApi Get(long id)
         {
-            return "value";
+            RespuestaApi resultado = new RespuestaApi();
+            List<TiposAnimales> tipo = new List<TiposAnimales>();
+            try
+            {
+                Db.Conectar();
+                if (Db.EstaLaConexionAbierta())
+                {
+                    tipo = Db.TiposPorId(id);
+
+                }
+                resultado.Error = "";
+                Db.Desconectar();
+            }
+            catch (Exception ex)
+            {
+                resultado.Error = "Te estoy petando Bro!";
+            }
+            resultado.TotalElemento = tipo.Count;
+            resultado.TiposAnimales = tipo;
+            return resultado;
+        }
+       
+        // POST: api/TipoAnimal
+        [HttpPost]
+        public RespuestaApi Post([FromBody]TiposAnimales TiposAnimales)
+        {
+            RespuestaApi respuesta = new RespuestaApi();
+            respuesta.Error = "";
+            int filasAfectadas = 0;
+            try
+            {
+                Db.Conectar();
+                if (Db.EstaLaConexionAbierta())
+                {
+                    filasAfectadas = Db.Agregaranimal(TiposAnimales);
+
+                }
+                respuesta.TotalElemento = filasAfectadas;
+                Db.Desconectar();
+            }
+            catch (Exception ex)
+            {
+                respuesta.TotalElemento = 0;
+                respuesta.Error = "Te estoy petando Bro!";
+            }
+
+            return (respuesta);
+
         }
 
-        // POST: api/TiposAnimales
-        public void Post([FromBody]string value)
+        // PUT: api/TipoAnimal/5
+        [HttpPut]
+        public RespuestaApi Put(int id, [FromBody]TiposAnimales TiposAnimales)
         {
+            RespuestaApi respuesta = new RespuestaApi();
+            respuesta.Error = "";
+            int filasAfectadas = 0;
+            try
+            {
+                Db.Conectar();
+                if (Db.EstaLaConexionAbierta())
+                {
+
+
+                    filasAfectadas = Db.Actualizartipos(id, TiposAnimales);
+
+
+
+                }
+                respuesta.TotalElemento = filasAfectadas;
+                Db.Desconectar();
+            }
+            catch (Exception ex)
+            {
+
+                respuesta.TotalElemento = 0;
+                respuesta.Error = "error al actualizar el Tipo";
+            }
+
+            return (respuesta);
+
+
+
         }
 
-        // PUT: api/TiposAnimales/5
-        public void Put(int id, [FromBody]string value)
+        // DELETE: api/TipoAnimal/5
+        [HttpDelete]
+        public RespuestaApi Delete(int id)
         {
-        }
+            RespuestaApi respuesta = new RespuestaApi();
+            respuesta.Error = "";
+            int filasAfectadas = 0;
+            try
+            {
+                Db.Conectar();
+                if (Db.EstaLaConexionAbierta())
+                {
+                    filasAfectadas = Db.Eliminartipo(id);
+                }
+                respuesta.TotalElemento = filasAfectadas;
+            }
+            catch (Exception ex)
+            {
 
-        // DELETE: api/TiposAnimales/5
-        public void Delete(int id)
-        {
+                respuesta.TotalElemento = 0;
+                respuesta.Error = "Te estoy pentando men";
+            }
+            return (respuesta);
         }
     }
 }
---
+++ ZooAzure/ZooAzure/assets/js/utilidades.js
@@ -0,0 +1,43 @@
+﻿
+type = ['', 'info', 'success', 'warning', 'error'];
+
+
+
+mensajes = {
+    showSwal: function (type, title, text) {
+        if (type == 'aviso') {
+            swal({
+                title: title,
+                text: text,
+                type: 'warning'
+            });
+            return;
+        }  if (type == 'error') {
+            swal({
+                title: title,
+                text: text,
+                type: 'error'
+            });
+            return;
+
+    }  if (type == 'exito') {
+        swal({
+            title: title,
+            text: text,
+            type: 'success'
+        });
+        return;
+    }
+    },
+  
+    dateToString: function (stringDate) {
+    var fechaFomateada = '';
+    var fecha = new Date(stringDate);
+    var dia = fecha.getDate();
+    var mes = (fecha.getMonth() + 1);
+    fechaFomateada = (dia < 10 ? '0' + dia : dia) + '/' + (mes < 10 ? '0' + mes : mes) + '/' + fecha.getFullYear();
+    return fechaFomateada;
+}
+
+}
+//las propiedades que me van a interesar 
