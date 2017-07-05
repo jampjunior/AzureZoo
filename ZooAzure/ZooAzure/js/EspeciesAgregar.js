075578d8f3b2ba459b9d9f57f2a5195e5d8dfa93
DESKTOP-5S71OSE\borja <jampdesigner@gmail.com>
2017-07-05 13:21:18 +0100

    terminado el especie ahora toca probarlo

--- ZooAzure/ZooAzure/js/EspeciesAgregar.js
+++ ZooAzure/ZooAzure/js/EspeciesAgregar.js
@@ -1,1 +1,41 @@
-﻿
+﻿$(document).ready(function () {
+    var id = window.location.search.substring(1).split('=')[1];
+    //Funicion para volver al listado
+    $('#btnCancelarespecie').click(function () {
+        window.location.href = '/Especies.html';
+    });
+
+    //Funcion para actualizar la marca
+    $('#btnAgregarespecie').click(function () {
+
+        var id = window.location.search.substring(1).split('=')[1];
+
+        // var id = 0; //de momento le ponemos un 0 es temporal
+        $.ajax({
+            url: `api/Especies`,
+            type: "POST",
+            dataType: 'json',
+            data: {
+
+                idEspecie: $('#especie').val(),
+                "clasificacion.idClasificacion": $('#clasificacion').val(),
+                "tipoAnimales.idTipoAnimal": $('#tipo').val(),
+                nombre: $('#nombre').val(),
+                nPatas: $('#pata').val(),
+                nMascota: $('#mascota').val(),
+            },
+            success: function (respuesta) {
+                // SI LLEGO HASTA AQUÍ QUIERE DECIR
+                // ME REDIRECCIONO A LA LISTA DE MARCAS
+                window.location.href = './Especies.html';
+            },
+            error: function (respuesta) {
+                console.log(respuesta);
+            }
+        });
+
+    });
+    //ejecuto la funcion que consultará los datos del api
+
+
+});
--- ZooAzure/ZooAzure/js/EspeciesEditar.js
+++ ZooAzure/ZooAzure/js/EspeciesEditar.js
@@ -1,1 +1,47 @@
-﻿
+﻿$(document).ready(function () {
+    // defino la funcion que consultara los datos del api
+    //function Cargardetalle() {
+    var id = window.location.search.substring(1).split('=')[1];
+
+
+
+
+    //Funicion para volver al listado
+    $('#btnCancelar').click(function () {
+        window.location.href = '/Especies.html';
+    });
+
+    //Funcion para actualizar la marca
+    $('#btnActualizar').click(function () {
+
+        var id = window.location.search.substring(1).split('=')[1];
+
+        // var id = 0; //de momento le ponemos un 0 es temporal
+        $.ajax({
+            url: `api/Especies/${id}`,
+            type: "PUT",
+            dataType: 'json',
+            data: {
+                
+                idEspecie: $('#especie').val(),
+                "clasificacion.idClasificacion": $('#clasificacion').val(),
+                "tipoAnimales.idTipoAnimal": $('#tipo').val(),
+                nombre: $('#nombre').val(),
+                nPatas: $('#pata').val(),
+                nMascota: $('#mascota').val(),
+            },
+            success: function (respuesta) {
+                // SI LLEGO HASTA AQUÍ QUIERE DECIR
+                // ME REDIRECCIONO A LA LISTA DE MARCAS
+                window.location.href = './Especies.html';
+            },
+            error: function (respuesta) {
+                console.log(respuesta);
+            }
+        });
+
+    });
+    //ejecuto la funcion que consultará los datos del api
+    // Cargardetalle();
+
+});
